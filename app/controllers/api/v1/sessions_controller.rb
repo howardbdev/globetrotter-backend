34f443fa9262caf5d60e05cc6623b7fee471e8bf
howardbdev <howarddevennish@gmail.com>
2019-06-07 22:52:00 -0400

    add sessions controller with create route

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
+  end
+
+end
