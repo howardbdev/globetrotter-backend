f82607cddfeeadd2d97074c6d0e76bd7a992e459
howardbdev <howarddevennish@gmail.com>
2019-06-07 22:45:51 -0400

    add routes around logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   # I want my api be globetrotter.com/api/v1/resource
   namespace :api do
     namespace :v1 do
