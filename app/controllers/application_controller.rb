0cafb0e027eb115a2a7db5e33051d284d0c399a8
howardbdev <howarddevennish@gmail.com>
2019-06-07 22:51:24 -0400

    include cookies and update current_user and login helpers around sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
-
+ include ::ActionController::Cookies
+ 
   def current_user
-    # currently a mocked version of "being logged in"
-    User.first
+    User.find_by(id: session[:user_id])
   end
 
   def logged_in?
