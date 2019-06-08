b06571aebf7ae4d22e0ea1b1b7ce7133b0b1c4b8
howardbdev <howarddevennish@gmail.com>
2019-06-07 22:50:54 -0400

    update application.js and cors configs to add cookies and sessions back in

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module GlobetrotterBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
+
   end
 end
