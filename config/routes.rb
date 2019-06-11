473d28d9634a3ccde852d427b9a1ab4ab2676fd6
howardbdev <howarddevennish@gmail.com>
2019-06-11 18:38:15 -0400

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   # I want my api be globetrotter.com/api/v1/resource
   namespace :api do
