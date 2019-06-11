041517fe37f934d5bdf745e0e7be62e0195b4cf8
howardbdev <howarddevennish@gmail.com>
2019-06-11 18:38:03 -0400

    add sessions#destory for logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -22,4 +22,11 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.clear
+    render json: {
+      notice: "successfully logged out"
+    }, status: :ok
+  end
+
 end
