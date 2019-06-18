55bebc069b31653eaa45a615f25981c6b6988eac
howardbdev <howarddevennish@gmail.com>
2019-06-18 19:43:12 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :attractions, through: :trips
   has_many :locations, through: :trips
   has_many :comments
+
+  validates :name, :username, :hometown, presence: true
+  validates :username, uniqueness: true
 end
