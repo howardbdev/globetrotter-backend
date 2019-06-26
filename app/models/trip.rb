f74a2bf113b0f178cc01903500c9290edfa430c5
howardbdev <howarddevennish@gmail.com>
2019-06-26 07:46:54 -0400

    add validations to trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :visits
   has_many :attractions, through: :visits
-  has_many :locations, through: :attractions 
+  has_many :locations, through: :attractions
+
+  validates :name, :user, presence: true
 end
