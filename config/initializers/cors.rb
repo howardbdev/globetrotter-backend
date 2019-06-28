9186844b2c3ad217a1abc40728323fe7e3c45e06
howardbdev <howarddevennish@gmail.com>
2019-06-28 10:14:27 -0400

    update cors for a couple different ports

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
 
     resource '*',
       headers: :any,
